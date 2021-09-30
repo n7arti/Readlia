dbf9dfac32990cd139e43acdf68291c552c1243b
Ader0m <78298703+Ader0m@users.noreply.github.com>
2021-09-30 09:59:04 +0300

    Create User.java

---
+++ User.java
@@ -0,0 +1,23 @@
+package com.Readlia;
+public class Book{
+  String email;
+  String pathSave;
+  
+   public string getPathSave() {
+        return pathSave;
+    }
+    
+    public void setPathSave(String pathSave) {
+        this.pathSave = pathSave;
+    }
+  public string getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void Athorizate(){
+      email = "dfgd@gmail.com";
+    }
+}
